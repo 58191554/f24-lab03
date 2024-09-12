f83ae5ac35f1f651bf26ddf6c3822561321b8c22
uid=tongzhen,ou=people,dc=hz,dc=netease,dc=com <tongzhen@corp.netease.com>
2024-09-12 13:47:58 -0400

    inherit add/addAll

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,27 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList{
     // Write your implementation below with API documentation
+    private int element_num = 0;
 
+    @Override
+    public boolean add(int num) {
+        this.element_num++;
+        return super.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));  // Use add() of InheritanceSortedIntList
+        }
+        return true;    
+    }
+
+    
+    public int getTotalAdded(){
+        System.out.printf("%d", this.element_num);
+        return this.element_num;
+    }
 }
