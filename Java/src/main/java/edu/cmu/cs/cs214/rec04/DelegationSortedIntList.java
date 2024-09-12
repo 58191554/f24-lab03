3b0f9e53794bce5f487bf634d2769085f454af4c
uid=tongzhen,ou=people,dc=hz,dc=netease,dc=com <tongzhen@corp.netease.com>
2024-09-12 13:50:04 -0400

    delegate implemented

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,50 +10,50 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-// public class DelegationSortedIntList implements IntegerList{
-//     // Write your implementation below with API documentation
-//     private final SortedIntList sortedlist;
-//     private int element_num = 0;
+public class DelegationSortedIntList implements IntegerList{
+    // Write your implementation below with API documentation
+    private final SortedIntList sortedlist;
+    private int element_num = 0;
 
-//     public DelegationSortedIntList(){
-//         this.sortedlist = new SortedIntList();
-//     }
+    public DelegationSortedIntList(){
+        this.sortedlist = new SortedIntList();
+    }
 
-//     @Override
-//     public boolean add(int num) {
-//         this.element_num++;
-//         return this.sortedlist.add(num);
-//     }
+    @Override
+    public boolean add(int num) {
+        this.element_num++;
+        return this.sortedlist.add(num);
+    }
 
-//     @Override
-//     public boolean addAll(IntegerList list) {
-//         for(int i = 0; i < list.size(); i++){
-//             this.add(list.get(i));
-//         }
-//         return true;
-//     }
+    @Override
+    public boolean addAll(IntegerList list) {
+        for(int i = 0; i < list.size(); i++){
+            this.add(list.get(i));
+        }
+        return true;
+    }
 
-//     @Override
-//     public int get(int index) {
-//         return this.sortedlist.get(index);
-//     }
+    @Override
+    public int get(int index) {
+        return this.sortedlist.get(index);
+    }
 
-//     @Override
-//     public int size() {
-//         return this.sortedlist.size();
-//     }
+    @Override
+    public int size() {
+        return this.sortedlist.size();
+    }
 
-//     @Override
-//     public boolean remove(int num) {
-//         return this.remove(num);
-//     }
+    @Override
+    public boolean remove(int num) {
+        return this.remove(num);
+    }
 
-//     @Override
-//     public boolean removeAll(IntegerList list) {
-//         return this.sortedlist.removeAll(list);
-//     }
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return this.sortedlist.removeAll(list);
+    }
 
-//     public int getTotalAdded(){
-//         return this.element_num;
-//     }
-// }
+    public int getTotalAdded(){
+        return this.element_num;
+    }
+}
